c807613f534f6fe83536ac415a734b183aabdbab
Roger Gammans <rgammans@gammascience.co.uk>
2016-09-24 12:10:33 +0100

    Update the code for single message and reset after play complete
     - This prevents it trying to play sample again; when it is already
       playing .

--- play_ongpio.py
+++ play_ongpio.py
@@ -2,22 +2,24 @@
  
 import os
 from time import sleep
- 
+import txlog
 import RPi.GPIO as GPIO
- 
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
-GPIO.setup(25, GPIO.IN)
+GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
+
+
+txlog.initialise_subsystem("DoorAlarm")
  
 while True:
-    if (GPIO.input(23) == False):
-        os.system('mpg123 -q binary-language-moisture-evaporators.mp3 &')
- 
-    if (GPIO.input(24) == False):
-        os.system('mpg123 -q power-converters.mp3 &')
- 
-    if (GPIO.input(25)== False):
-        os.system('mpg123 -q vader.mp3 &')
- 
-    sleep(0.1);
+    #Wait for a change in the PIR sensor
+    GPIO.wait_for_edge((23, GPIO.RISING)
+    txlog.send_msg("Door alarm triggered")
+    os.system('mpg123 -q binary-language-moisture-evaporators.mp3')
+
+    #Clear the flip-flop latch; after playback has finished
+    GPIO.output(24, GPIO.HIGH)
+    sleep(0.001);
+    GPIO.output(24, GPIO.LOW)
+
