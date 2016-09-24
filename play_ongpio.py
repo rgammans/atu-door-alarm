61c81616015da7d35c1653909adcf3ddf533e7e9
Roger Gammans <rgammans@gammascience.co.uk>
2016-09-24 14:23:30 +0000

    Update  after testing.    Add debug mode.    Note- Clear pin is active low.    Call shellscript to ensure correct audio routing settings

--- play_ongpio.py
+++ play_ongpio.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python
  
+DEBUG=1
+
 import os
 from time import sleep
 import txlog
 import RPi.GPIO as GPIO
 
+#Setup Input GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
 
+##Enable audio routing.
+os.system('~/setup_jack.sh')
 
+##Annouce we are running and clear the input latch.
 txlog.initialise_subsystem("DoorAlarm")
+GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
+sleep(0.001);
+GPIO.output(24, GPIO.HIGH)
+
  
 while True:
     #Wait for a change in the PIR sensor
-    GPIO.wait_for_edge((23, GPIO.RISING)
+    GPIO.wait_for_edge(23, GPIO.RISING)
+
+    #Report alarm fired to game log server.
     txlog.send_msg("Door alarm triggered")
-    os.system('mpg123 -q binary-language-moisture-evaporators.mp3')
+    if DEBUG:
+       print "Edge detected waiting playing audio";
+
+    ##Play the audio
+    os.system('mpg123 -q ~/audio/binary-language.mp3')
+
+    if DEBUG:
+       print "waiting one before clearing";
+       sleep(1)
+       print "cleared latch"
 
     #Clear the flip-flop latch; after playback has finished
-    GPIO.output(24, GPIO.HIGH)
-    sleep(0.001);
     GPIO.output(24, GPIO.LOW)
+    sleep(0.001);
+    GPIO.output(24, GPIO.HIGH)
 
